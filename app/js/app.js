d66b032ce919ee84869e0f5157b3ef4f7568ec6d
Ed Rooth <ed.rooth@rackspace.com>
2013-01-18 17:51:45 -0800

    commit

--- app/js/app.js
+++ app/js/app.js
@@ -2,9 +2,24 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
-  config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: MyCtrl1});
-    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: MyCtrl2});
-    $routeProvider.otherwise({redirectTo: '/view1'});
+angular.module('dashboard',
+  ['dashboard.filters', 'dashboard.services', 'dashboard.directives'])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/entities', {
+      templateUrl: 'partials/entity-list.html',
+      controller: EntityListCtrl
+    });
+    $routeProvider.when('/entities/:entityId/checks', {
+      templateUrl: 'partials/check-list.html',
+      controller: CheckListCtrl
+    });
+    $routeProvider.when('/entities/:entityId/alarms', {
+      templateUrl: 'partials/alarm-list.html',
+      controller: AlarmListCtrl
+    });
+    $routeProvider.when('/entities/:entityId/alarms/:alarmId', {
+      templateUrl: 'partials/alarm-detail.html',
+      controller: AlarmDetailCtrl
+    });
+    $routeProvider.otherwise({ redirectTo: '/entities' });
   }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,11 +2,50 @@
 
 /* Controllers */
 
+var BASE_URL = 'http://localhost:8080/';
 
-function MyCtrl1() {}
-MyCtrl1.$inject = [];
-
-
-function MyCtrl2() {
+function EntityListCtrl($scope, $http) {
+  delete $http.defaults.headers.common['X-Requested-With'];
+  $scope.loadStatus = 'loading...';
+  $http.get(BASE_URL + 'entities').success(function(data) {
+    $scope.entities = data.values;
+    $scope.loadStatus = 'loaded';
+  });
 }
-MyCtrl2.$inject = [];
+EntityListCtrl.$inject = ['$scope', '$http'];
+
+function CheckListCtrl($scope, $http, $routeParams) {
+  var url = BASE_URL + 'entities/' + $routeParams.entityId + '/checks';
+  delete $http.defaults.headers.common['X-Requested-With'];
+  $scope.loadStatus = 'loading...';
+  $http.get(url).success(function(data) {
+    $scope.checks = data.values;
+    $scope.loadStatus = 'loaded';
+  });
+  $scope.entityId = $routeParams.entityId;
+}
+CheckListCtrl.$inject = ['$scope', '$http', '$routeParams'];
+
+function AlarmListCtrl($scope, $http, $routeParams) {
+  var url = BASE_URL + 'entities/' + $routeParams.entityId + '/alarms';
+  delete $http.defaults.headers.common['X-Requested-With'];
+  $scope.loadStatus = 'loading...';
+  $http.get(url).success(function(data) {
+    $scope.alarms = data.values;
+    $scope.loadStatus = 'loaded';
+  });
+  $scope.entityId = $routeParams.entityId;
+}
+AlarmListCtrl.$inject = ['$scope', '$http', '$routeParams'];
+
+function AlarmDetailCtrl($scope, $http, $routeParams) {
+  var url = BASE_URL + 'entities/' + $routeParams.entityId + '/alarms/' +
+    $routeParams.alarmId;
+  delete $http.defaults.headers.common['X-Requested-With'];
+  $scope.loadStatus = 'loading...';
+  $http.get(url).success(function(data) {
+    $scope.alarm = data;
+    $scope.loadStatus = 'loaded';
+  });
+}
+AlarmDetailCtrl.$inject = ['$scope', '$http', '$routeParams'];
